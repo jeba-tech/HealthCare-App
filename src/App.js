401806826e2d6983d35008b64fabda43387b0378
JEBA <fawjiajeba@gmail.com>
2022-10-14 17:42:59 +0600

    project initilized and components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Services from './Components/Services/Services';
+import About from './Components/About/About';
+import Dashboard from './Components/Dashboard/Dashboard';
+import ContactUs from './Components/Contact/ContactUs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Services" element={<Services />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/Contact" element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+      return (
+            <div>
+                  <div className='nav'>
+                        <div className='web-name'>
+                              <p>Health Care</p>
+                        </div>
+
+                        <Link to="/" className='link'>Home</Link>
+                        <Link to="/Services" className='link'>Services</Link>
+                        <Link to="/About" className='link'>About Us</Link>
+                        <Link to="/Dashboard" className='link'>Dashboard</Link>
+                        <Link to="/Contact" className='link'>Contact Us</Link>
+
+
+
+
+                  </div>
+            </div>
+      );
+};
+
+export default Header;
